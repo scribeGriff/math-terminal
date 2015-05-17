f10cdb0a2c10b7ea175ce9662d3247cc8bc3bc3b
Richard Griffith <scribe.griff@gmail.com>
2015-05-17 15:26:40 -0500

    added sawtooth and triangle wave gens

--- js/wavegen.js
+++ js/wavegen.js
@@ -1,6 +1,8 @@
 /* global math: false */
 /* jshint node: true, browser: true, esnext: true */
 
+// TODO: Refactor to remove redundant code
+
 "use strict";
 
 (function () {
@@ -39,6 +41,40 @@
         } else {
           _samples[i] = _amplify * _preamp;
         }
+        _sample += _step;
+      }
+
+      return _samples;
+    },
+    
+    sawtoothwave: function sawtoothwave(amp, cycles, slength) {
+      var _amplitude = amp !== undefined ? amp : 1,
+          _cycles = cycles !== undefined ? cycles : 1,
+          _samples_length = slength !== undefined ? slength : 500,
+          _samples = new Array(_samples_length),
+          _sample = 0,
+          _scale = -(2 / Math.PI) * _amplitude,
+          _step = Math.PI * _cycles / _samples_length;
+      
+      for (var i = 0; i < _samples_length; i++) {
+        _samples[i] = _scale * Math.atan(1 / Math.tan(_sample));
+        _sample += _step;
+      }
+
+      return _samples;
+    },
+    
+    trianglewave: function trianglewave(amp, cycles, slength) {
+      var _amplitude = amp !== undefined ? amp : 1,
+          _cycles = cycles !== undefined ? cycles : 1,
+          _samples_length = slength !== undefined ? slength : 500,
+          _samples = new Array(_samples_length),
+          _sample = 0,
+          _scale = (2 / Math.PI) * _amplitude,
+          _step = Math.PI * 2 * _cycles / _samples_length;
+      
+      for (var i = 0; i < _samples_length; i++) {
+        _samples[i] = _scale * Math.asin(Math.sin(_sample));
         _sample += _step;
       }
 
